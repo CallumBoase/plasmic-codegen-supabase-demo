e49b531b3fd7e309220af9a8ddcad15030ea3284
CallumBoase <callum.boase@gmail.com>
2025-03-28 15:52:59 +1100

    supabaseuserglobalcontext

--- pages/plasmic-host.tsx
+++ pages/plasmic-host.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { PlasmicCanvasHost, registerComponent } from '@plasmicapp/react-web/lib/host';
+import { PlasmicCanvasHost, registerComponent, registerGlobalContext } from '@plasmicapp/react-web/lib/host';
 
-import { SupabaseProvider, SupabaseProviderMeta } from 'plasmic-supabase';
+import { SupabaseProvider, SupabaseProviderMeta, SupabaseUserGlobalContext, SupabaseUserGlobalContextMeta } from 'plasmic-supabase';
 
 // You can register any code components that you want to use here; see
 // https://docs.plasmic.app/learn/code-components-ref/
@@ -11,6 +11,8 @@ import { SupabaseProvider, SupabaseProviderMeta } from 'plasmic-supabase';
 // https://docs.plasmic.app/learn/app-hosting/#set-a-plasmic-project-to-use-your-app-host
 
 // registerComponent(...)
+
+registerGlobalContext(SupabaseUserGlobalContext, SupabaseUserGlobalContextMeta);
 registerComponent(SupabaseProvider, SupabaseProviderMeta);
 
 export default function PlasmicHost() {
